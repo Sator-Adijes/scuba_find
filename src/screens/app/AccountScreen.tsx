c54cf3467b3a2634531297f971d669fb850856d8
Satiflex <adijes.developpement@gmail.com>
2026-06-12 10:27:04 +0200

    Modification du style du projet

---
+++ src/screens/app/AccountScreen.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import { View, Text } from 'react-native';
+
+export const AccountScreen = (): React.JSX.Element => {
+  return (
+    <View className="flex-1 items-center justify-center bg-white px-lg">
+      <Text className="text-h4 font-bold text-green-900 mb-xs">Je t'aime fefemmme</Text>
+      <Text className="text-b1 text-neutral-600">
+        Trouvez les meilleurs boobs de Montreuil City Zoo
+      </Text>
+    </View>
+  );
+}
